203a7e5a0acebc1ffe0a6e19029630c53c5203d7
MeantToBee <113875558+MeantToBee@users.noreply.github.com>
2022-11-13 21:35:16 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -108,13 +108,13 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '九九',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       // 使用其他通道时，请严格按照各个通道的教程进行填写
       id: 'ofw5z6Gt4a_Z0RgCsEa8V8BbcfPc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'AsBz8Da_UnOhmBAhlKGZegAtQkyNXiguKCB-HgFL1Sk',
+      useTemplateId: 'WkFSo9AsZwkJVFdpIj3K-0FV81Z6d6mO6OCSmu_NBOY',
       // 所在省份或城市，也可以不填
       province: '湖北',
       // 所在城市或县区
